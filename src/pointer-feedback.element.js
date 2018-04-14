76421f06c35e06746de854421492d9de09b06994
Adam Argyle <atom@argyleink.com>
2018-04-13 21:05:10 -0700

    still combing

--- src/pointer-feedback.element.js
+++ src/pointer-feedback.element.js
@@ -1,47 +1,49 @@
-const STYLES = `
-  :host {
-    display: block;
-    position: absolute;
-    top: 0; left: 0; right: 0; bottom: 0;
-    overflow: hidden;
-    z-index: -1;
-    contain: layout;
-    --ink-ripple_accent-color: hsl(0,0%,80%);
-    --ink-ripple_scale-speed: 300ms;
-    --ink-ripple_transparency-speed: 200ms;
-  }
+const $STYLES = `
+  <style>
+    :host {
+      display: block;
+      position: absolute;
+      top: 0; left: 0; right: 0; bottom: 0;
+      overflow: hidden;
+      z-index: -1;
+      contain: layout;
+      --pointer-feedback_accent-color: hsl(0,0%,80%);
+      --pointer-feedback_scale-speed: 300ms;
+      --pointer-feedback_transparency-speed: 200ms;
+    }
 
-  :host::before {
-    position: absolute;
-    display: block;
-    content: '';
-    border-radius: 50%;
-    background: var(--ink-ripple_accent-color);
-    opacity: 0;
-    transform: scale(0);
-  }
+    :host::before {
+      position: absolute;
+      display: block;
+      content: '';
+      border-radius: 50%;
+      background: var(--pointer-feedback_accent-color);
+      opacity: 0;
+      transform: scale(0);
+    }
 
-  :host([animatable])::before {
-    transition: opacity var(--ink-ripple_transparency-speed) linear, transform var(--ink-ripple_scale-speed) linear;
-  }
+    :host([animatable])::before {
+      transition: opacity var(--pointer-feedback_transparency-speed) linear, transform var(--pointer-feedback_scale-speed) linear;
+    }
 
-  :host([mouseup][animatable])::before {
-    transition: opacity .4s linear, transform .2s linear;
-  }
+    :host([mouseup][animatable])::before {
+      transition: opacity .4s linear, transform .2s linear;
+    }
 
-  :host([mousedown])::before {
-    opacity: 1;
-    transform: scale(1);
-  }
+    :host([mousedown])::before {
+      opacity: 1;
+      transform: scale(1);
+    }
 
-  :host([mouseup]:not([mousedown]))::before {
-    opacity: 0;
-    transform: scale(1);
-  }
+    :host([mouseup]:not([mousedown]))::before {
+      opacity: 0;
+      transform: scale(1);
+    }
 
-  :host([hidden]) {
-    display: none;
-  }
+    :host([hidden]) {
+      display: none;
+    }
+  </style>
 `
 // todo: test getting boundingrect once, and only once
 class PointerFeedback extends HTMLElement {
@@ -49,30 +51,10 @@ class PointerFeedback extends HTMLElement {
   constructor() {
     super()
 
-    this.attachShadow({
-      mode: 'open'
-    })
-
-    this.shadowRoot
-      .appendChild(PointerFeedback.template.cloneNode(true))
-
-    this.styles   = this.shadowRoot.querySelector('style')
-    this.cssAdded = false
-  }
-
-  static get template() {
-    if (this.$fragment) 
-      return this.$fragment
-
-    const $fragment     = document.createDocumentFragment()
-    let $styles         = document.createElement('style')
-    $styles.innerHTML   = STYLES
-
-    $fragment.appendChild($styles)
-
-    this.$fragment = $fragment
-
-    return $fragment
+    this.$shadow            = this.attachShadow({ mode: 'open' })
+    this.$shadow.innerHTML  = $STYLES
+    this.styles             = this.$shadow.querySelector('style')
+    this.cssAdded           = false
   }
 
   set animatable(val) {
@@ -109,36 +91,26 @@ class PointerFeedback extends HTMLElement {
       this._triggerRippleIn(offsetX, offsetY)
     })
 
-    document.documentElement.addEventListener('mouseup', e => {
+    this.addEventListener('mouseup', e => {
       if (this.disabled) return
       this._triggerRippleOut()
     })
 
-    this.parentElement.style.overflow   = 'hidden' // todo: try matching parent border radius
-    this.parentElement.style.willChange = 'transform'
+    // ensure parent element is in it's own index stack 
+    this.parentElement.style.willChange   = 'transform'
+    this._matchParentShape()
   }
 
-  simulateRipple(x = false, y = false) {
-    if (!x || !y) {
-      const rect = this.getBoundingClientRect()
-      x = rect.width / 2
-      y = rect.height / 2
-    }
+  _matchParentShape() {
+    const parentStyle = getComputedStyle(this.parentElement, null).getPropertyValue('border-radius')
 
-    this._triggerRippleIn(x, y)
-
-    this.addEventListener('transitionend', e => {
-      if (e.propertyName === 'transform')
-        this._triggerRippleOut()
-    })
+    if (parentStyle != '0px')
+      this.style.borderRadius = parentStyle
   }
 
   _fadeOut() {
     this.mousedown = false
-
-    requestAnimationFrame(() => {
-      this.addEventListener('transitionend', this._transitionOutEnd)
-    })
+    this.addEventListener('transitionend', this._transitionOutEnd)
   }
 
   _removeCSS() {
@@ -147,9 +119,9 @@ class PointerFeedback extends HTMLElement {
   }
 
   _reset() {
-    this.animatable = false
-    this.mousedown = false
-    this.mouseup = false
+    this.animatable   = false
+    this.mousedown    = false
+    this.mouseup      = false
 
     this.removeEventListener('transitionend', this._transitionOutEnd)
     this.removeEventListener('transitionend', this._transitionInEnd)
@@ -161,14 +133,12 @@ class PointerFeedback extends HTMLElement {
   }
 
   _transitionInEnd(evt) {
-    if (evt.pseudoElement) {
-      if (evt.propertyName === 'transform' && !this.transitionInOver) {
-        this.removeEventListener('transitionend', this._transitionInEnd)
+    if (evt.pseudoElement && evt.propertyName === 'transform' && !this.transitionInOver) {
+      this.removeEventListener('transitionend', this._transitionInEnd)
 
-        this.transitionInOver = true
+      this.transitionInOver = true
 
-        if (this.mouseup) this._fadeOut()
-      }
+      if (this.mouseup) this._fadeOut()
     }
   }
 
@@ -180,29 +150,25 @@ class PointerFeedback extends HTMLElement {
   }
 
   _positionPseduoElement(x, y) {
-    let { height, width } = this.getBoundingClientRect()
+    // force recalc so previous animation doesn't transition to the new state
+    const { height, width } = this.getBoundingClientRect()
 
-    const largest = Math.max(height, width)
+    const largest           = Math.max(height, width)
+    const fullCoverage      = largest * 2.1 + (largest / 2.1)
 
-    width   = largest * 2 + (largest / 2)
-    height  = largest * 2 + (largest / 2)
+    const xPos              = x - (fullCoverage / 2)
+    const yPos              = y - (fullCoverage / 2)
 
-    const xPos  = x - (width / 2)
-    const yPos  = y - (height / 2)
-
-    let speed = largest
-
-    if (speed > 700) speed = 700
-    if (speed < 300) speed = 300
+    const speed             = Math.max(300, Math.min(700, largest))
 
     this.styles.sheet.insertRule(`
       :host:before {
         left:   ${xPos}px;
         top:    ${yPos}px;
-        width:  ${width}px;
-        height: ${height}px;
-        --ink-ripple_scale-speed: ${speed}ms;
-        --ink-ripple_transparency-speed: ${speed / 3}ms;
+        width:  ${fullCoverage}px;
+        height: ${fullCoverage}px;
+        --pointer-feedback_scale-speed: ${speed}ms;
+        --pointer-feedback_transparency-speed: ${speed / 3}ms;
       }
     `, 0)
 
@@ -213,11 +179,10 @@ class PointerFeedback extends HTMLElement {
     this._reset()
     this._positionPseduoElement(offsetX, offsetY)
 
-    requestAnimationFrame(() => {
-      this.addEventListener('transitionend', this._transitionInEnd)
-      this.animatable   = true
-      this.mousedown    = true
-    })
+    this.animatable   = true
+    this.mousedown    = true
+
+    this.addEventListener('transitionend', this._transitionInEnd)
   }
 
   _triggerRippleOut() {
